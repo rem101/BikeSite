240af920982f7e5a6a3a5d59642191493b028937
rem101 <rob@meroconsulting.com>
2023-06-08 07:17:49 -0400

    auditing warnings

--- BikeSite.Dal/Models/Bike.cs
+++ BikeSite.Dal/Models/Bike.cs
@@ -5,9 +5,15 @@ public class Bike
 {
     public int Id { get; set; }
     public Guid BikeId { get; set; }
+    //remove nullable suggestion
+    #pragma warning disable CS8618
     public string Model { get; set; }
+    #pragma warning restore CS8618
     public int FrameSize { get; set; }
     public decimal Price { get; set; }
     public int BikeBrandId { get; set; }
+    //remove nullable suggestion
+    #pragma warning disable CS8618
     public BikeBrand Brand { get; set; }
+    #pragma warning restore CS8618
 }
--- BikeSite.Dal/Models/BikeBrand.cs
+++ BikeSite.Dal/Models/BikeBrand.cs
@@ -4,5 +4,8 @@ public class BikeBrand
 {
     public int Id { get; set; }
     public Guid BrandId { get; set; }
+    //remove nullable suggestion
+    #pragma warning disable CS8618
     public string Brand { get; set; }
+    #pragma warning restore CS8618
 }
